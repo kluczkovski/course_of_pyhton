16ecb35ddfbe9d47931ca76c835024b855a61917
ekluczkovski <kluczkovski@hotmail.com>
2018-01-24 20:22:41 -0800

    operation

---
+++ aula_4_variaveisEoperadores.py
@@ -0,0 +1,28 @@
+
+print("Test using variables and arithmateics operators")
+
+number1 = input("Enter first number: \n ")
+number1 = int(number1)
+number2 = input("Enter second number: \n ")
+number2 = int(number2)
+
+sum = number1 + number2
+
+print("The sum is ", sum)
+
+
+subtraction = number1 - number2
+print("The subtraction is ", subtraction)
+
+
+multiplication = number1 * number2
+print("The multiplication is ", multiplication)
+
+division = number1 / number2
+print("The Division is", division)
+
+exponentiation = number1 ** number2
+print("The exponentiation is ", exponentiation)
+
+modulus = number1 % number2
+print("The modulus is ", modulus)
